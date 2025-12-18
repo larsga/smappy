8d26a5915e1473dc1ce335be9995da0de4cdf7f8
Lars Marius Garshol <larsga@garshol.priv.no>
2025-12-18 13:50:24 +0100

    Better documentation and tests

--- README.md
+++ README.md
@@ -1,10 +1,99 @@
 
 # smappy
 
-A simple mapping toolkit for Python. Can produce Google Maps in
-HTML/JavaScript, as well as static maps in PNG and PDF format.
-Supports Shapefiles and GeoJSON input.
+A simple mapping engine in pure Python3 with a Python API. Can produce
+static maps in PNG and PDF format, as well as Google Maps in
+HTML/JavaScript.  Supports Shapefiles, GeoJSON, and GeoTIFF input.
 
-Todo:
+Still in early phases of development, but usable.
 
-  * Support SVG maps.
+Features:
+
+  * Configurable maps based on Natural Earth data.
+  * Configurable display of Shapefiles and GeoJSON from any source.
+  * Adding markers, optionally with text labels.
+  * Adding text labels without markers.
+  * Raster data overlay.
+  * Choropleth maps.
+  * Legends.
+
+## Natural Earth examples
+
+If you first download and unzip [Natural
+Earth](https://www.naturalearthdata.com) shapefiles into the folder
+`/foo`, running this script:
+
+```
+from smappy import prefab
+
+view = prefab.map_views['nordic']
+themap = prefab.build_natural_earth(view, '/foo')
+themap.render_to('demo')
+```
+
+Will produce this map in PNG format:
+
+You can load a TOML file to change the layout (this file is built-in):
+
+```
+from smappy import prefab
+
+view = prefab.map_views['nordic']
+map_style = prefab.load_map_style('grayscale')
+themap = prefab.build_natural_earth(view, '/foo')
+themap.render_to('demo')
+```
+
+That will produce this map in PNG format:
+
+If you download and unzip
+[ETOPO1](https://www.ncei.noaa.gov/products/etopo-global-relief-model)
+elevation data into the same folder, you can also add elevations:
+
+```
+from smappy import prefab
+
+view = prefab.map_views['nordic']
+themap = prefab.build_natural_earth(view, '/foo', elevation = True)
+themap.render_to('demo')
+```
+
+Here is the map with elevations:
+
+The maps can also be decorated with text labels and markers:
+
+```
+import os, csv, sys
+from smappy import prefab, mapbase
+
+map_style = prefab.default_map_style
+map_style.set_border_line_width(2.5)
+
+view = prefab.map_views['germany']
+themap = prefab.build_natural_earth(view, os.environ['SHAPEDIR'],
+                                    map_style = map_style,
+                                    elevation = True)
+
+place_text = mapbase.TextStyle(
+   font_size = 29,
+    font_name = 'DejaVuSerif.ttf',
+    font_color = 'black',
+    halo_color = 'white',
+    halo_radius = 3,
+)
+
+marker = mapbase.Marker(
+    '#FFFF00',
+    text_style = place_text,
+    title_display = mapbase.TitleDisplay.NEXT_TO_SYMBOL
+)
+
+for row in csv.DictReader(open('first-lager-brewery.csv'), delimiter = ';'):
+    lat = float(row['Lat'])
+    lng = float(row['Long'])
+    year = int(row['Year'])
+
+themap.render_to('lager-map')
+```
+
+The output looks like this:
--- test/test.py
+++ test/test.py
@@ -1,8 +1,18 @@
 
 import unittest, tempfile, os
-from smappy import mapbase, googlemap
+from pathlib import Path
+from PIL import Image
+from smappy import mapbase, googlemap, prefab
 
 SHAPEDIR = os.environ.get('SHAPEDIR') # shapefiles must be located here
+ROOT = Path(__file__).parent
+MIN_SIMILARITY = 5
+
+class BlobCache:
+
+    def get_blob(self, name):
+        blob = ROOT / 'blob-cache' / name
+        return blob
 
 class TestMaps(unittest.TestCase):
 
@@ -23,21 +33,57 @@ class TestMaps(unittest.TestCase):
             inf = open(tstfile)
             thefile = inf.read()
             self.assertTrue('GOOGLE_MAPS_KEY' in thefile)
+            self.assertTrue('Oslo' in thefile)
+            self.assertTrue('Helsinki' in thefile)
             inf.close()
 
-    @unittest.skip('need to figure out how to install pymapnik3')
-    def test_simple_mapnik(self):
-        import prefab
+    def test_simple_native_png(self):
         with tempfile.TemporaryDirectory() as tmpdir:
-            tstfile = tmpdir + '/' + 'tst.png'
+            tstfile = tmpdir + '/' + 'tst'
 
-            view = prefab.map_views['nordic']
-            themap = prefab.build_natural_earth(view, SHAPEDIR)
-
-            m1 = mapbase.Marker('black')
-            themap.add_marker(59, 20, 'Oslo', m1)
-
-            m2 = mapbase.Marker('white')
-            themap.add_marker(60, 30, 'Helsinki', m2)
-
+            rivers = [
+               ('name', 'Rhine'),
+               ('name', 'Main'),
+            ]
+            view = prefab.MapView(west = 5, east = 28, north = 56, south = 46,
+                                  width = 2000, height = 1600)
+            themap = prefab.build_natural_earth(view, SHAPEDIR, rivers = rivers)
             themap.render_to(tstfile)
+
+            base = cache.get_blob('simple-native.png')
+            self.assertTrue(img_eq(base, tstfile + '.png'))
+
+    def test_elevation_native_png(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tstfile = tmpdir + '/' + 'tst'
+
+            view = prefab.MapView(east = 30, west = 4, south = 54.5, north = 65,
+                                  width = 1800, height = 1400)
+            themap = prefab.build_natural_earth(view, SHAPEDIR,
+                                                elevation = True)
+            themap.render_to(tstfile)
+
+            base = cache.get_blob('elevation-native.png')
+            self.assertTrue(img_eq(base, tstfile + '.png'))
+
+def img_eq(f1, f2):
+    return img_diff(f1, f2) < MIN_SIMILARITY
+
+def img_diff(f1, f2):
+    img1 = Image.open(f1)
+    img2 = Image.open(f2)
+
+    if img1.width != img2.width or img1.height != img2.height:
+        return 1_000_000_000
+
+    total = 0
+    for (p1, p2) in zip(img1.getdata(), img2.getdata()):
+        total += abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]) + abs(p1[2] - p2[2])
+
+    length = len(img1.getdata())
+
+    img1.close()
+    img2.close()
+    return total / (length * 3)
+
+cache = BlobCache()
